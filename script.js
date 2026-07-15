431259abe0c9e1e88b6ae38336db9646f4b6cdaa
Essame Manuela <etordiessamebrindamanuela@gmail.com>
2026-07-15 18:08:30 +0200

    Première version de mon portfolio

---
+++ script.js
@@ -0,0 +1,56 @@
+// ==========================
+// MODE SOMBRE
+// ==========================
+
+const themeButton = document.getElementById("theme-toggle");
+
+themeButton.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    const icon = themeButton.querySelector("i");
+
+    if(document.body.classList.contains("dark-mode")){
+
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+
+    }else{
+
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+
+    }
+
+});
+// ==============================
+// Apparition progressive des sections
+// ==============================
+
+const sections = document.querySelectorAll("section");
+
+sections.forEach(section=>{
+    section.classList.add("hidden");
+});
+
+const observer = new IntersectionObserver(entries=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+    threshold:0.2
+});
+
+sections.forEach(section=>{
+
+    observer.observe(section);
+
+});
